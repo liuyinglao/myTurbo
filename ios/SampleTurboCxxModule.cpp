ed9ac063742018fe732225fed25153c315807daa
liuyinglao <lyl940427@gmail.com>
2021-12-28 12:35:47 -0500

    try ios pure c++ turbo module

---
+++ ios/SampleTurboCxxModule.cpp
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include "SampleTurboCxxModule.hpp"
+
+#include <ReactCommon/TurboModuleUtils.h>
+
+using namespace facebook;
+
+namespace facebook {
+namespace react {
+
+SampleTurboCxxModule::SampleTurboCxxModule(
+    std::shared_ptr<CallInvoker> jsInvoker)
+    : NativeSampleTurboCxxModuleSpecJSI(jsInvoker) {}
+
+jsi::String SampleTurboCxxModule::getString(
+    jsi::Runtime &rt,
+    const jsi::String &arg) {
+  return jsi::String::createFromUtf8(rt, arg.utf8(rt));
+}
+
+jsi::Object SampleTurboCxxModule::getConstants(jsi::Runtime &rt) {
+  // Note: return type isn't type-safe.
+  jsi::Object result(rt);
+  result.setProperty(rt, "const1", jsi::Value(true));
+  result.setProperty(rt, "const2", jsi::Value(375));
+  result.setProperty(
+      rt, "const3", jsi::String::createFromUtf8(rt, "something"));
+  return result;
+}
+
+} // namespace react
+} // namespace facebook
---
+++ ios/SampleTurboCxxModule.hpp
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+//#ifndef SampleTurboCxxModule_hpp
+//#define SampleTurboCxxModule_hpp
+//
+//#include <stdio.h>
+//
+//#endif /* SampleTurboCxxModule_hpp */
+
+#pragma once
+
+#include <memory>
+
+#include "React-Codegen/SampleTurboCxxModule/SampleTurboCxxModule.h"
+
+namespace facebook {
+namespace react {
+
+/**
+ * A sample implementation of the C++ spec. In practice, this class can just
+ * extend jsi::HostObject directly, but using the spec provides build-time
+ * type-safety.
+ */
+class SampleTurboCxxModule : public NativeSampleTurboCxxModuleSpecJSI {
+ public:
+  SampleTurboCxxModule(std::shared_ptr<CallInvoker> jsInvoker);
+  jsi::String getString(jsi::Runtime &rt, const jsi::String &arg) override;
+  jsi::Object getConstants(jsi::Runtime &rt) override;
+};
+
+} // namespace react
+} // namespace facebook
